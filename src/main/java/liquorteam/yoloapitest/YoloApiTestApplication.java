941534dbcf713c9a952ee500d81558fadeef134e
Red-Gunny <nehepkldi@naver.com>
2022-08-30 23:58:49 +0900

    Add : 초기 프로젝트 구성

---
+++ src/main/java/liquorteam/yoloapitest/YoloApiTestApplication.java
@@ -0,0 +1,13 @@
+package liquorteam.yoloapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoloApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoloApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/liquorteam/yoloapitest/YoloApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package liquorteam.yoloapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoloApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
